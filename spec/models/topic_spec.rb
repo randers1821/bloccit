9c11eb46aa045e3aa6e47f8a9d1e5eab2863d2f1
Rachel <raedesignz1@gmail.com>
2016-12-08 08:59:32 -0500

    Assignment 23 complete

--- spec/models/topic_spec.rb
+++ spec/models/topic_spec.rb
@@ -1,13 +1,15 @@
 require 'rails_helper'
+include RandomData
 
 RSpec.describe Topic, type: :model do
 
-  let(:name) { RandomData.random_sentence }
-  let(:description) { RandomData.random_paragraph }
-  let(:public) { true }
-  let(:topic) { Topic.create!(name: name, description: description) }
+  let(:topic) { Topic.create!(name: RandomData.random_sentence, description: RandomData.random_paragraph) }
 
   it { is_expected.to have_many(:posts) }
+  it { should validate_length_of(:name).is_at_least(5) }
+    it { should validate_length_of(:description).is_at_least(15) }
+    it { should validate_prescence_of(:name) }
+    it { should validate_prescence_of(:description) }
 
   describe "attributes" do
     it "has name, description, and public attributes" do
