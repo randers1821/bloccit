2150ce064068c1ab2530234b6629d7abf67e53b7
Rachel <raedesignz1@gmail.com>
2016-12-12 10:05:16 -0500

    assignment complete

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
+  resources :sponsored_posts
   resources :topics do
     resources :posts, except: [:index]
-  end 
+    
+  end
 
   get 'about' => 'welcome#about'
 
---
+++ spec/views/sponsored_posts/index.html.erb_spec.rb
@@ -0,0 +1,28 @@
+require 'rails_helper'
+
+RSpec.describe "sponsored_posts/index", type: :view do
+  before(:each) do
+    assign(:sponsored_posts, [
+      SponsoredPost.create!(
+        :title => "Title",
+        :body => "MyText",
+        :price => 2,
+        :topic => ""
+      ),
+      SponsoredPost.create!(
+        :title => "Title",
+        :body => "MyText",
+        :price => 2,
+        :topic => ""
+      )
+    ])
+  end
+
+  it "renders a list of sponsored_posts" do
+    render
+    assert_select "tr>td", :text => "Title".to_s, :count => 2
+    assert_select "tr>td", :text => "MyText".to_s, :count => 2
+    assert_select "tr>td", :text => 2.to_s, :count => 2
+    assert_select "tr>td", :text => "".to_s, :count => 2
+  end
+end
