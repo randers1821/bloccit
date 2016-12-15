db3a7062663ffbf99d5b4bff42d27da8cdcb0eeb
Rachel <raedesignz1@gmail.com>
2016-12-15 14:04:33 -0500

    checkpoint complete

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,10 @@ Rails.application.routes.draw do
     resources :posts, except: [:index]
   end
 
+  resources :posts, only: [] do
+    resources :comments, only: [:create, :destroy]
+  end 
+
   resources :users, only: [:new, :create]
 
   get "users/confirmation"
