2c97f4e1ffef886a2839061b7b27c7e2a0c24e57
Rachel <raedesignz1@gmail.com>
2016-12-19 12:42:41 -0500

    assignment 28 complete

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,8 +1,8 @@
 class PostsController < ApplicationController
 
 before_action :require_sign_in, except: :show
-
-before_action :authorize_user, except: [:show, :new, :create]
+before_action :authorize_moderator, only: [:update, :edit, :create]
+before_action :authorize_user, only: [:delete]
 
   def show
 
@@ -64,6 +64,15 @@ end
 
   def post_params
     params.require(:post).permit(:title, :body)
+  end
+
+  def authorize_moderator
+    post = Post.find(params[:id])
+
+    unless current_user == post.user || current_user.admin? || current_user.moderator?
+      flash[:alert] = "You must be an admin to do that."
+      redirect_to [post.topic, post]
+    end
   end
 
   def authorize_user
